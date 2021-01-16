2efb588abf1d9db03dead750de2b7d92b8be3f6e
Marcos Paulo Simioni <mpsimioni@hotmail.com>
2021-01-16 15:04:53 -0300

    Fist class: Position

---
+++ src/applicacao/Programa.java
@@ -0,0 +1,14 @@
+package applicacao;
+
+import tabuleiro.Posicao;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Posicao pos = new Posicao(3, 5);
+		System.out.println(pos);
+
+	}
+
+}
